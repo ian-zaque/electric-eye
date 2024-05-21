f2a4c654656740537ffb2648497c0859e5810f6e
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2024-05-21 14:11:33 -0300

    Att de dependências

--- eye-beholder/resources/js/app.js
+++ eye-beholder/resources/js/app.js
@@ -5,41 +5,43 @@
  */
 
 require('./bootstrap');
-import App from './App.vue'
-import router from './router'
+import App from './AppUser.vue'
+import VueRouter from 'vue-router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import VueSidebarMenu from 'vue-sidebar-menu'
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 window.Vue = require('vue');
+
+Vue.use(BootstrapVue);
+Vue.use(VueRouter);
+Vue.use(IconsPlugin);
 Vue.use(VueSidebarMenu)
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    hashbang: false,
+    routes: [
+        // { path: '/user/atividades', name: 'userAtividades', component: AtribuicoesTable, props: { hasFiltro: true } },
+        // { path: '/:pathMatch(.*)', name: 'NotFound', component: BoardColaborador }              // Redireciona NotFoundUrl para BoardColaborador
+    ]
+})
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 //App
-Vue.component('app-container', require('./App.vue').default);
+Vue.component('app-user', require('./AppUser.vue').default);
 
 // Sidebar
 Vue.component("side-bar", require("./components/Sidebar/Sidebar.vue").default);
 Vue.component("side-bar-link", require("./components/Sidebar/SidebarLink.vue").default);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
 const app = new Vue({
+    router,
+    store,
     el: '#app',
 });
