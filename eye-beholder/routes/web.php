0ec9d0786ab3c848f6c02c9d708e27e5e2c59844
Ian Zaque Santos <ianzaque.uefs@gmail.com>
2024-06-04 13:47:17 -0300

    Feat.: Redirecionamento em 404
    Caso dê erro 404, URL not found, redireciona para homepage

--- eye-beholder/routes/web.php
+++ eye-beholder/routes/web.php
@@ -19,10 +19,12 @@ use Illuminate\Support\Facades\Route;
 
 // Rota de controle do Vue Router
 // Redireciona para objeto VueRouter (.\resources\js\app.js) e impede de gerar 404 Not Found
-// Redireciona NotFoundUrl para BoardColaborador
-Route::get('/{vue?}', 'HomeController@index')->where('vue', '[\/\w\.-]*');
+// Redireciona NotFoundUrl para Home
+// Route::get('/{vue?}', 'HomeController@index')->where('vue', '[\/\w\.-]*');
 
 Auth::routes(['verify' => true]);
 
-Route::get('/', 'HomeController@index')->name('home')->middleware('verified');;
-Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');;
+Route::get('/', 'HomeController@index')->name('home')->middleware('verified');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
+
+Route::any('{query}', function() { return redirect('/'); })->where('query', '.*');
