8ffaf4f2c97c4c3ef23e67250370a40ee59ae9ba
Ian Zaque Santos <ianzaque.uefs@gmail.com>
2024-06-06 15:26:32 -0300

    Update index.js

--- eye-beholder/resources/js/router/index.js
+++ eye-beholder/resources/js/router/index.js
@@ -1,22 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../Home/Home.vue'
-import TypeSensorsPage from '../components/TypeSensors/TypeSensorsPage.vue'
-import SensorsPage from '../components/Sensors/SensorsPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  // { path: '/', name: 'home', component: Home },
-  // { path: '/login', name: 'login', component: Home },
-  // { path: '/logout', name: 'logout', component: Home },
-  // { path: '/sensors-types', name: 'Tipos de Sensores', component: TypeSensorsPage },
-  // { path: '/sensors', name: 'Sensores', component: SensorsPage },
 ]
 
 
 export default new VueRouter({
-  // mode: 'history',
-  // base: process.env.BASE_URL,
   routes
 })
