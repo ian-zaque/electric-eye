451950145db7f8db419c7378bea0997254f8ddbc
Ian Zaque Santos <ianzaque.uefs@gmail.com>
2024-08-10 12:19:53 -0300

    Changing app name

--- eye-beholder/resources/views/welcome.blade.php
+++ eye-beholder/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Beholder</title>
+        <title>Electric Eye</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Eye of the Beholder
+                    Electric Eye
                 </div>
 
                 <div class="links">
