fccdd7c49c8ffe50bc2f37d7162ed3f84f968ff0
Ian Zaque Santos <ianzaque.uefs@gmail.com>
2024-10-10 16:24:32 -0300

    Update MqttPublisherService.php

--- eye-beholder/app/Services/MqttPublisherService.php
+++ eye-beholder/app/Services/MqttPublisherService.php
@@ -13,24 +13,8 @@ class MqttPublisherService
 
         \App\Jobs\SubscribeUdeProcess::withChain([
             new UpdateNodeTableProcess,
+            // new ConfigProcess,
         ])
         ->dispatch($topic, $id_mac_json_data);
     }
-
-    // private function publishQoS0($topic, $data){
-    //     // dd($topic, $data);
-    //     MQTT::publish($topic, $data);
-    // }
-
-    // private function publishQoS1($topic, $data){
-    //     $mqtt = MQTT::connection();
-    //     $mqtt->publish($topic, $data, 1, true);
-    //     $mqtt->loop(true);
-    // }
-
-    // private function publishQoS2($topic, $data) {
-    //     $mqtt = MQTT::connection();
-    //     $mqtt->publish($topic, $data, 2, true); // Retain the message
-    //     $mqtt->loop(true);
-    // }
 }
