157d58bbd26fcafc5f8019913e55289322c61525
Ian Zaque Santos <ianzaque.uefs@gmail.com>
2024-11-08 20:57:25 -0300

    Mac_ID correto

--- eye-beholder/database/seeds/UdeSeeder.php
+++ eye-beholder/database/seeds/UdeSeeder.php
@@ -17,8 +17,8 @@ class UdeSeeder extends Seeder
             $ude = new Ude();
             $ude->class_id = 1;
             $ude->interest_zone_id = 1;
-            $ude->name = 'UDE CN-1';
-            $ude->mac_id = '46-0A-CE-E4-E3-18';
+            $ude->name = 'UDE-1 Cidade Nova';
+            $ude->mac_id = '46:0A:CE:E4:E3:18';
             $ude->latitude = '-12.22451';
             $ude->longitude = '-38.96423';
             $ude->save();
@@ -26,8 +26,8 @@ class UdeSeeder extends Seeder
             $ude = new Ude();
             $ude->class_id = 2;
             $ude->interest_zone_id = 2;
-            $ude->name = 'UDE SI-1';
-            $ude->mac_id = '3A-6B-A4-C2-19-3B';
+            $ude->name = 'UDE-2 Sim';
+            $ude->mac_id = '3A:6B:A4:C2:19:3B';
             $ude->latitude = '-12.25577';
             $ude->longitude = '-38.92097';
             $ude->save();
@@ -35,8 +35,8 @@ class UdeSeeder extends Seeder
             $ude = new Ude(); 
             $ude->class_id = 3;
             $ude->interest_zone_id = 3;
-            $ude->name = 'UDE TO-1';
-            $ude->mac_id = 'A8-DB-21-6F-7F-7F';
+            $ude->name = 'UDE-3 Tomba';
+            $ude->mac_id = 'A8:DB:21:6F:7F:7F';
             $ude->latitude = '-12.28720';
             $ude->longitude = '-38.95764';
             $ude->save();
@@ -44,8 +44,8 @@ class UdeSeeder extends Seeder
             $ude = new Ude();
             $ude->class_id = 2;
             $ude->interest_zone_id = 4;
-            $ude->name = 'UDE SO-1';
-            $ude->mac_id = '92-C7-7C-E8-4A-E4';
+            $ude->name = 'UDE-4 Sobradinho';
+            $ude->mac_id = '92:C7:7C:E8:4A:E4';
             $ude->latitude = '-12.24351';
             $ude->longitude = '-38.97292';
             $ude->save();
